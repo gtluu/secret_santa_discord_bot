2ff82de4070dd05046401d8883b46a83ba96eb87
Andrew Bryant <andrew.bryant98@protonmail.com>
2020-01-14 00:35:19 -0500

    Bare minimum for compatibility with the discord.py rewrite

--- santa_pepe.py
+++ santa_pepe.py
@@ -3,7 +3,6 @@ import discord
 import random
 from collections import deque
 
-# secret santa randomization algorithm
 def pairup(people):
     random.shuffle(people)
     partners = deque(people)
@@ -18,47 +17,41 @@ if __name__ == "__main__":
 
     @client.event
     async def on_message(message):
-        # insert server id
-		server = client.get_guild(id='')
+        guild = client.get_guild(id=)
         if message.author != client.user:
-			# message list of santas entered
             if message.content == ".santas":
-                for member in server.members:
+                for member in guild.members:
                     for role in member.roles:
                         if role.name == "Santa":
                             print(member)
-                            await client.send_message(message.channel, member)
-			# start secret santa
+                            await message.channel.send(member)
             if message.content == ".santa bot activate":
                 people = []
-                for member in server.members:
+                for member in guild.members:
                     for role in member.roles:
                         if role.name == "Santa":
                             people.append(member)
                 santas = pairup(people)
-                for i in santas:
-                    print("Santa: " + i[0].name)
-                    print("Receiver: " + i[1].name)
+                for pair in santas:
+                    print("Santa: " + pair[0].name)
+                    print("Receiver: " + pair[1].name)
                     print('\n')
-                    await client.send_message(i[0], santa_message)
-                    await client.send_message(i[0], "You are " + str(i[1]) + "'s santa.")
-			# tell pepe that your job is done
+                    await pair[0].send(santa_message)
+                    await pair[0].send("You are " + str(pair[1]) + "'s santa.")
             if message.content == ".gifted":
                 retired_santas_txt = os.path.dirname(os.path.abspath(__file__)) + "\giftedbois.txt"
                 with open(retired_santas_txt, 'r+') as file_obj:
                     if str(message.author).strip() in file_obj.read().strip():
-                        await client.send_message(message.channel, "You've already been added to the list, bucko.")
+                        await message.channel.send("You've already been added to the list, bucko.")
                         
                     elif str(message.author) not in file_obj.read().strip():
-                        await client.send_message(message.channel, "You are hereby retired from your role as Santa, %s" % str(message.author)[:-5])
+                        await message.channel.send("You are hereby retired from your role as Santa, %s" % str(message.author)[:-5])
                         file_obj.write("\n" + str(message.author))
-                await client.send_message(message.channel, "Now, The following Santas have finished their job:\n")
+                await message.channel.send("Now, The following Santas have finished their job:\n")
                 with open(retired_santas_txt, 'r') as file_obj:
                     for line in file_obj:
-                        await client.send_message(message.channel, line + "\n")
-			# test message
+                        await message.channel.send(line + "\n")
             if message.content == ".test":
                 print("Hello Pepe!")
 
-	# insert Discord server key
     client.run('')
